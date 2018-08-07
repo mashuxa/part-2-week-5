de881a84e1f762a7b355d96fff4f9b37eb510634
Your Name <you@example.com>
2018-08-08 00:22:32 +0300

    done

--- script.js
+++ script.js
@@ -1,57 +1,91 @@
-'use strict';
-
+'use strict'; 
 function validateForm(settings) {
     var form = document.getElementById(settings.formId);
-    var isValidForm = false;
+    var inputs = document.getElementsByTagName("input");
 
+   
+    //    функция которая делает проверку одного инпута
     function check(elem) {
-        console.log(elem);
-        var isInvalidInput = false;
+        var val = elem.value;
+        var isValid = true;
 
-        //проверка элемента. если элемент верно заполнен вернуть true
+        //проверка на required
+        if ((elem.dataset.required !== undefined) && (val.trim() === "")) {
+            elem.classList.add(settings.inputErrorClass);
+            return false;
+        }
 
-        if (isInvalidInput) {
+
+        switch (elem.dataset.validator) {
+            case "letters":
+                if (val.match(/^([a-zа-яё]*)$/ig) === null) {
+                    isValid = false;
+                }
+                break;
+            case "number":
+                if (val.match(/^(-?\d*)$/ig) === null) {
+                    isValid = false;
+                } else {
+                    if ((elem.dataset.validatorMax !== undefined) && (Number(val) > Number(elem.dataset.validatorMax))) {
+                        isValid = false;
+                    } else if ((elem.dataset.validatorMin !== undefined) && (Number(val) < Number(elem.dataset.validatorMin))) {
+                        isValid = false;
+                    }
+
+                }
+
+                break;
+            case "regexp":
+                if (val.match(elem.dataset.validatorPattern) === null) {
+                    isValid = false;
+                }
+                break;
+        } 
+        
+        
+        if (!isValid) {
             elem.classList.add(settings.inputErrorClass);
         }
+        
+        
+        return isValid;
     }
 
 
-
-
-
-
     //отмена стандартной отправки формы
-    //проверка всех инпутов
+    //проверка всех инпутов по enter или кнопке
     form.addEventListener("submit", function (e) {
         e.preventDefault();
-        var elements = document.getElementsByTagName("input");
-        for (var i = 0; i < elements.length; i++) {
-            check(elements[i]);
+        var formClass = settings.formValidClass;
 
+        for (var i = 0; i < inputs.length; i++) {
+            if (!check(inputs[i])) {
+                formClass = settings.formInvalidClass;
+            }
         }
+        form.classList.remove(settings.formValidClass);
+        form.classList.remove(settings.formInvalidClass);
+        form.classList.add(formClass);
     }, true);
-
 
 
     //потеря фокуса на инпуте
+    //проверка теекущего инпута при потере фокуса
     form.addEventListener("blur", function (e) {
-        if (e.target.tagName === "INPUT") {
-            check(e.target);
+        var currentInput = e.target;
+        if (currentInput.tagName === "INPUT") {
+            check(currentInput);
         }
     }, true);
 
 
-    //при фокусе на инпуте 
+    //при фокусе на инпуте
+    //удалить класс с ошибкой
     form.addEventListener("focus", function (e) {
         if (e.target.tagName === "INPUT") {
             e.target.classList.remove(settings.inputErrorClass);
         }
     }, true);
-
-
-
-
-
 
 
 }
